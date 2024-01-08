1beffdf20100db93f3df7674422202290e78d8b5
Rhys Rushton <rhysie@tutanota.com>
2024-01-08 16:22:42 +1100

    react learning

--- React-UDEMY-Router-/React-Essentials/01-starting-project/src/App.jsx
+++ React-UDEMY-Router-/React-Essentials/01-starting-project/src/App.jsx
@@ -1,15 +1,64 @@
+import { CORE_CONCEPTS } from "./Data";
+import reactImg from './assets/react-core-concepts.png'
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+
+  let indexInt = genRandomInt(reactDescriptions.length)
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[indexInt]} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+const CoreConcept = ({ title, description, img }) => {
+
+  return (
+    <li>
+      <img src={img} alt="..."></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+
+
+}
+
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              img={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              img={CORE_CONCEPTS[2].image}
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
